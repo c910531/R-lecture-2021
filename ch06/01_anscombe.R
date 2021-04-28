f015e6a85440657fed5130474d77c038c48e2432
c910531 <c910531@gmail.com>
2021-04-28 11:32:12 +0900

    ch06 단원문제

--- ch06/01_anscombe.R
+++ ch06/01_anscombe.R
@@ -10,9 +10,115 @@ apply(anscombe, 2, var)
 cor(anscombe$x1, anscombe$y1)
 cor(anscombe$x2, anscombe$y2)
 
+
+### for 반복 구문을 이용한 반복 출력
 for (i in 1:4) {
     print(cor(anscombe[,i], anscombe[,i+4]))
 }
 
 # 요약
 summary(anscombe)
+
+# 그래프 그리기
+library(ggplot2)
+
+ggplot(anscombe) +
+    geom_point(aes(x4,y4),color='darkorange',size=3) +
+    # scale_x_continuous(breaks=seq(2,20,2)) +
+    # scale_y_continuous(breaks=seq(2,14,2)) +
+    xlim(2,20) +
+    ylim(2,14) +
+    geom_abline(slope = 0.5, intercept = 3,
+                color='cornflowerblue',size=1) +
+    labs(title = 'Dataset 1')
+
+# gridExtra (그래프 하나씩을 변수명으로 지정 후 한번에 출력)
+
+p1 <- ggplot(anscombe) +
+    geom_point(aes(x1,y1),color='darkorange',size=3) +
+    # scale_x_continuous(breaks=seq(2,20,2)) +
+    # scale_y_continuous(breaks=seq(2,14,2)) +
+    xlim(2,20) +
+    ylim(2,14) +
+    geom_abline(slope = 0.5, intercept = 3,
+                color='cornflowerblue',size=1) +
+    labs(title = 'Dataset 1')
+p1
+
+p2 <- ggplot(anscombe) +
+    geom_point(aes(x2,y2),color='darkorange',size=3) +
+    # scale_x_continuous(breaks=seq(2,20,2)) +
+    # scale_y_continuous(breaks=seq(2,14,2)) +
+    xlim(2,20) +
+    ylim(2,14) +
+    geom_abline(slope = 0.5, intercept = 3,
+                color='cornflowerblue',size=1) +
+    labs(title = 'Dataset 2')
+p2
+
+p3 <- ggplot(anscombe) +
+    geom_point(aes(x3,y3),color='darkorange',size=3) +
+    # scale_x_continuous(breaks=seq(2,20,2)) +
+    # scale_y_continuous(breaks=seq(2,14,2)) +
+    xlim(2,20) +
+    ylim(2,14) +
+    geom_abline(slope = 0.5, intercept = 3,
+                color='cornflowerblue',size=1) +
+    labs(title = 'Dataset 3')
+p3
+
+p4 <- ggplot(anscombe) +
+    geom_point(aes(x4,y4),color='darkorange',size=3) +
+    # scale_x_continuous(breaks=seq(2,20,2)) +
+    # scale_y_continuous(breaks=seq(2,14,2)) +
+    xlim(2,20) +
+    ylim(2,14) +
+    geom_abline(slope = 0.5, intercept = 3,
+                color='cornflowerblue',size=1) +
+    labs(title = 'Dataset 4')
+
+p4
+
+library(gridExtra)
+grid.arrange(p1,p2,p3,p4, ncol=2, top="Anscombe's Quartet")
+
+figures <- list()
+figures <- append(figures, p1)
+figures <- append(figures, p2)
+figures <- append(figures, p3)
+figures <- append(figures, p4)
+figures[1]
+
+for (i in 1:4) {
+    
+}
+
+############################################################
+
+# Source Refactoring
+x <-ggplot(anscombe) +
+    geom_point(aes(x4,y4),color='darkorange',size=3)
+x
+assign(paste('m',4,sep='.'), x)
+grid.arrange(p1,p2,p3,p4, ncol=2, top="Anscombe's Quartet")
+
+library(dplyr)
+for (i in 1:4) {
+    x_data <- anscombe[,i]
+    y_data <- anscombe[,4+i]
+    x <-ggplot(anscombe) +
+        geom_point(aes(x_data, y_data),
+                   color='darkorange',size=3) +
+        # scale_x_continuous(breaks=seq(2,20,2)) +
+        # scale_y_continuous(breaks=seq(2,14,2)) +
+        xlim(2,20) +
+        ylim(2,14) +
+        geom_abline(slope = 0.5, intercept = 3,
+                    color='cornflowerblue',size=1) +
+        labs(title = paste0('Dataset',i),
+             x=paste0('x',i),y=paste0('y',i))
+    assign(paste('m', i, sep='.'),x)
+    rm(x)
+}
+
+grid.arrange(m.1,m.2,m.3,m.4, ncol=2, top="Anscombe's Quartet")
--- ch06/단원문제.R
+++ ch06/단원문제.R
@@ -6,11 +6,12 @@ library(gapminder)
 #### 1) 1952~2007년 중 1952년도의 인구 구성을 추출한 후 시각화하라
 x = gapminder %>%  
     filter(year==1952) %>% 
-    select(continent, pop) %>% 
+    select(country, pop) %>%
+    arrange(desc(pop)) %>% 
     head()
 
-pie(x$pop, x$continent)
-barplot(x$pop, names.arg=x$continent)
+pie(x$pop, x$country)
+barplot(x$pop, names.arg=x$country)
 
 #### 2) 1952~2007년의 인구 구성을 for 문을 이용해 반복적으로 시각화하라
 x = gapminder %>%
@@ -31,11 +32,54 @@ x <- gapminder %>%
 pie(x$gdp, x$country)
 barplot(x$gdp, names.arg=x$country)
 
+## 2) 정답 (# 숫자 초과로 인한 표기를 as.numeric통해 오류 정정) 
+for (i in seq(1952,2007,5)) {
+    print(i)
+    x <- gapminder %>% 
+        filter(year==i) %>% 
+        select(country, pop) %>% 
+        arrange(desc(pop)) %>% 
+        head()
+    pie(as.numeric(x$pop), x$country)
+    barplot(x$pop, names.arg=x$country)
+    title(i)
+}
+
+
 #### 3) 시각화 그래프를 통해 알게 된 사실을 몇 가지로 요약하라
 
 ### 02. ggplot2를 이용하여 Iris 데이터 셋에 대해서 다음 문제를 푸세요.
 
+# 2-1 airquality
+head(airquality)
+library(tidyr)
+air_tidy <- gather(airquality, key='Measure', value='Value',
+                   -Day, -Month)
+head(air_tidy)
+tail(air_tidy)
+dim(airquality)
+dim(air_tidy)
+
+air_tidy %>% 
+    ggplot(aes(Day, Value, col=Measure)) +
+    geom_point() +
+    facet_wrap(~Month)
+
+# 정답
+#2-2 iris
+
+iris_tidy <- gather(iris, key='feat', value='value',
+                    -Species)
+head(iris_tidy)
+tail(iris_tidy)
+iris_tidy %>% 
+    ggplot(aes(feat, value, col=Species)) +
+    geom_point(position='jitter') # jitter -> 중복값의 중복 표기 방지
+
 #### 1) 품종별로 Sepal/Petal의 Length, Width 산점도 그리기. (총 6개)
+### 항목이 여러개일때 x축 y축 2가지 분류로 나눈 후 그래프 구성하면 쉽다 (tidyr 패키지 활용)
+
+
 iris %>% 
     filter(Species=='setosa') %>%
     ggplot(aes(x=Sepal.Length, y=Sepal.Width)) +
@@ -48,31 +92,32 @@ iris %>%
     geom_point(alpha=0.2) +
     ggtitle("Setosa Petal Length-Width")
 
-iris %>% 
-    filter(Species=='versicolor') %>%
-    ggplot(aes(x=Sepal.Length, y=Sepal.Width)) +
-    geom_point(alpha=0.2) +
-    ggtitle("Versicolor Sepal Length-Width")
+# 정답
+seto <- filter(iris, Species=='setosa')
+vers <- filter(iris, Species=='versicolor')
+virg <- filter(iris, Species=='virginica')
 
-iris %>% 
-    filter(Species=='versicolor') %>%
-    ggplot(aes(x=Petal.Length, y=Petal.Width)) +
-    geom_point(alpha=0.2) +
-    ggtitle("Versicolor Petal Length-Width")
+seto_s <- seto %>% 
+    ggplot(aes(Sepal.Length,Sepal.Width,col=Species)) +
+    geom_point()
+seto_p <- seto %>% 
+    ggplot(aes(Petal.Length,Petal.Width,col=Species)) +
+    geom_point()
+vers_s <- vers %>% 
+    ggplot(aes(Sepal.Length,Sepal.Width,col=Species)) +
+    geom_point()
+vers_p <- vers %>% 
+    ggplot(aes(Petal.Length,Petal.Width,col=Species)) +
+    geom_point()
+virg_s <- virg %>% 
+    ggplot(aes(Sepal.Length,Sepal.Width,col=Species)) +
+    geom_point()
+virg_p <- virg %>% 
+    ggplot(aes(Petal.Length,Petal.Width,col=Species)) +
+    geom_point()
 
-iris %>% 
-    filter(Species=='virginica') %>%
-    ggplot(aes(x=Sepal.Length, y=Sepal.Width)) +
-    geom_point(alpha=0.2) +
-    ggtitle("Virginica Sepal Length-Width")
-
-iris %>% 
-    filter(Species=='virginica') %>%
-    ggplot(aes(x=Petal.Length, y=Petal.Width)) +
-    geom_point(alpha=0.2) +
-    ggtitle("Virginica Petal Length-Width")
-
-
+grid.arrange(seto_s,seto_p,vers_s,vers_p,
+             virg_s,virg_p,ncol=2)
 
 #### 2) 품종별 Sepal/Petal의 Length/Width 평균을 비교하되 항목을 옆으로 늘어놓은 것(beside=T)과 위로 쌓아올린 것 2개를 그리시오. 
 #### (총 12개 항목의 데이터를 2개의 그래프에)
@@ -95,5 +140,29 @@ iris %>%
 ggplot(iris, aes(x=Sepal.Length, y=Sepal.Width, col=Species)) +
     geom_bar(stat='identity')
 
-#### 3) 박스 플롯 그리기
-boxplot(iris)
+# 정답 barplot + legend
+seto_mean <- apply(iris[iris$Species=='setosa',1:4],2,mean)
+vers_mean <- apply(iris[iris$Species=='versicolor',1:4],2,mean)
+virg_mean <- apply(iris[iris$Species=='virginica',1:4],2,mean)
+mean_of_iris <- rbind(seto_mean, vers_mean, virg_mean)
+mean_of_iris
+
+barplot(mean_of_iris, beside=T,
+        main='품종별 평균', ylim=c(0,8), col=c('red','green','blue'))
+legend('topright',
+       legend=c('Setosa','Versicolor','Virginica'),
+       fill=c('red','green','blue'),
+       cex=0.5)
+################################################################
+df <- iris %>% 
+    group_by(Species) %>% 
+    summarise(sepal_length=mean(Sepal.Length), sepal_width=mean(Sepal.Width),
+              petal_length=mean(Petal.Length), petal_width=mean(Petal.Width))
+df
+df_tidy <- gather(df, key='Feature', value='Value', -Species)
+df_tidy
+ggplot(df_tidy, aes(x=Feature, y=Value, fill=Species)) +
+    geom_bar(stat='identity',position='dodge') +
+    coord_flip() 
+### 3) 박스 플롯 그리기
+par(mfrow=c(3,1))   # mfrow=c( , ) = 3행 1열의 그래프
