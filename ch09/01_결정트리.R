9efb44b9ab2375594389591414f426869d321933
c910531 <c910531@gmail.com>
2021-05-03 11:32:25 +0900

    'ch08'

---
+++ ch09/01_결정트리.R
@@ -0,0 +1,47 @@
+# 분류(Classification)
+# 결정 트리(Decision Tree)
+library(rpart)
+head(iris)
+
+dtc <- rpart(Species ~., iris)  # iris 데이터를 결정 트리로 학습
+summary(dtc)
+dtc
+
+# 결정트리 시각화
+par(mfrow=c(1,1), xpd=NA)
+plot(dtc)
+text(dtc, use.n=T)
+
+# 예측
+pred <- predict(dtc, iris, type='class')
+table(pred, iris$Species)
+
+# 평가
+library(caret)  # install.packages('caret') 설치 후 불러오기
+confusionMatrix(pred,iris$Species)
+
+# 시각화
+library(rpart.plot)
+ibrary(ggplot2)
+rpart.plot(dtc)
+rpart.plot(dtc, type=4)
+
+# 훈련 / 테스트 셋으로 분리하여 시행
+set.seed(2021)
+iris_index <- sample(1:nrow(iris), 0.8*nrow(iris))
+iris_train <- iris[iris_index, ]
+iris_test <- iris[setdiff(1:nrow(iris), iris_index),]
+iris_test <- iris[-iris_index,] # 윗 지정 식과 동일한 결과값 출
+dim(iris_train)
+dim(iris_test)
+table(iris_train$Species)
+table(iris_test$Species)
+
+# 모델링
+dtc <- rpart(Species~.,iris_train)
+
+# 예측
+pred <- predict(dtc, iris_test, type='class')
+
+# 평가
+confusionMatrix(pred, iris_test$Species)
