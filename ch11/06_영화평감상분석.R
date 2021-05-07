e7e1332858ba774e55efa8ba274933d6db979a71
c910531 <c910531@gmail.com>
2021-05-07 13:02:32 +0900

    'ch11'

---
+++ ch11/06_영화평감상분석.R
@@ -0,0 +1,110 @@
+# IMDB 영화평 감상분석
+library(text2vec)
+library(caret)
+library(tm)
+
+str(movie_review)
+
+# 8:2 비율로 훈련 / 테스트 데이터 셋 분할
+set.seed(2021)
+train_list <- createDataPartition(y=movie_review$sentiment,
+                                  p=0.8, list=F)
+mtrain <- movie_review[train_list,]
+mtest <- movie_review[-train_list,]
+
+# 훈련 데이터셋에 대해 DTM 구축
+# 테스트 데이터셋에 대해서도 동일하게 적용해야 함
+doc <- Corpus(VectorSource(mtrain$review))
+doc <- tm_map(doc, content_transformer(tolower))
+doc <- tm_map(doc, removeNumbers)
+stop_words <- c(stopwords('en'), '<br />')
+doc <- tm_map(doc, removeWords, stop_words)
+doc <- tm_map(doc, removePunctuation)
+doc <- tm_map(doc, stripWhitespace)
+
+dtm <- DocumentTermMatrix(doc, control=list(weighting=weightTf))
+
+dim(dtm)                          
+dtm[1,]
+inspect(dtm)
+
+dtm_tfidf <- DocumentTermMatrix(doc,
+                                control=list(weighting=weightTfIdf))
+inspect(dtm_tfidf)
+
+# 모델링 가능한 형태로 DTM을 변환
+dtm_small_tfidf <- removeSparseTerms(dtm_tfidf, 0.9)
+dim(dtm_small_tfidf)
+
+# Sentiment(y)와 DTM을 묶어서 데이터 프레임을 생성
+X <- as.matrix(dtm_small_tfidf)
+dataTrain <- as.data.frame(cbind(mtrain$sentiment, X))
+head(dataTrain)
+colnames(dataTrain)[1] <- 'y'
+dataTrain$y <- as.factor(dataTrain$y)
+
+# Decision Tree로 학습
+library(rpart)
+dt <- rpart(y ~ ., dataTrain)
+
+# 테스트 데이터셋으로 모델 성능 평가
+docTest <- Corpus(VectorSource(mtest$review))
+doc <- tm_map(doc, content_transformer(tolower))
+doc <- tm_map(doc, removeNumbers)
+doc <- tm_map(doc, removeWords, stop_words)
+doc <- tm_map(doc, removePunctuation)
+doc <- tm_map(doc, stripWhitespace)
+
+dtmTest <- DocumentTermMatrix(docTest,
+                              control=list(dictionary=dtm_small$dimnames$Terms))
+dim(dtmTest)
+inspect(dtmTest)
+
+# Sentiment(y)와 DTM_Test를 묶어서 데이터 프레임을 생성
+X <- as.matrix(dtmTest)
+dataTest <- as.data.frame(cbind(mtest$sentiment, X))
+head(dataTest)
+colnames(dataTest)[1] <- 'y'
+dataTest$y <- as.factor(dataTest$y)
+
+# 학습했던 모델로 예측
+dt_pred <- predict(dt, dataTest, type='class')
+table(dt_pred, dataTest$y)
+
+###################
+# SVM 으로 훈련
+###################
+library(e1071)
+svc <- svm(y ~., dataTrain)
+sv_pred <- predict(svc, dataTest, type='class')
+table(sv_pred, dataTest$y)
+
+###################
+# Tf-Idf로 변환
+###################
+library(rpart)
+dtm_tfidf <- DocumentTermMatrix(doc,
+                                control=list(weighting=weightTfIdf))
+inspect(dtm_tfidf)
+dataTrain <- as.data.frame(cbind(mtrain$sentiment, X))
+head(dataTrain)
+colnames(dataTrain)[1] <- 'y'
+dataTrain$y <- as.factor(dataTrain$y)
+
+# Decision Tree로 학습
+dt_tfidf <- rpart(y ~ ., dataTrain)
+
+# Test dataset
+dtmTest_tfidf <- DocumentTermMatrix(docTest,
+                control=list(dictionary=dtm_small_tfidf$dimnames$Terms,
+                             weighting=weightTfIdf))
+
+X <- as.matrix(dtmTest_tfidf)
+dataTest <- as.data.frame(cbind(mtest$sentiment, X))
+head(dataTest)
+colnames(dataTest)[1] <- 'y'
+dataTest$y <- as.factor(dataTest$y)
+
+# 예측
+dt_pred_tfidf <- predict(dt_tfidf, dataTest, type='class')
+table(sv_pred, dataTest$y)
