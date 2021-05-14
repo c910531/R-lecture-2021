7869714c411fb6ae8cca418cea7fcbeb4184effa
c910531 <c910531@gmail.com>
2021-05-14 09:08:19 +0900

    '프로젝트'

--- 프로젝트/code_d_0512.R
+++ 프로젝트/code_d_0512.R
@@ -1,19 +1,32 @@
 getwd()
 library(dplyr)
 library(ggplot2)
+library(gapminder)
+
+t_trade<- read.csv('data/trade/total_trade.csv')
+t_trade
+View(t_trade)
+t_g <- t_trade %>%
+    group_by(시군구,계약연도) %>% 
+    ggplot(aes(계약연도,t_거래금액,group=시군구,col=시군구)) + 
+    geom_point(stat='identity',alpha=0.2, position='jitter') +
+    scale_y_log10()
+t_g
 
 
-d_trade <- read.csv('data/trade/d_trade.csv')
-d_trade
+t_g_b <- t_trade %>%
+    group_by(시군구,계약연도) %>% 
+    ggplot(aes(계약연도,t_거래금액,group=시군구,col=시군구)) + 
+    geom_bar(stat='identity', position='dodge',aes(fill=시군구))  
+t_g_b
 
+summarise(avg_t_trade = mean(전용면적),avg_mon=mean(거래금액),
+          +               avg_avg=avg_mon/avg_t_trade) 
+dun_avg_16
 
-summary(d_trade)
-View(d_trade)
-
-d_trade %>% 
-    filter(계약연도==2017) %>% 
-    select(시군구, 전용면적,거래금액) %>% 
-    summarise(avg_d_trade = mean(전용면적),avg_mon=mean(거래금액),
-              avg_avg=avg_mon/avg_d_trade) %>% 
-    ggplot(aes(avg_d_trade, avg_mon)) +
-    geom_boxplot()
+t_trade %>% 
+    group_by(시군구,계약연도,거래금액) %>% 
+    summarise(avg_t_trade = mean(전용면적),avg_mon=mean(거래금액),
+              avg_avg=avg_mon/avg_t_trade) %>% 
+    ggplot(aes(계약연도,avg_avg,group=시군구,col=시군구)) +
+    geom_bar(stat='identity',position='jitter')
